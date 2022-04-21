16b1416ae139e7fed25f4a2f4aed40086a3acccb
Rick Kauffman <rick@rickkauffman.com>
2022-04-21 10:58:12 -0700

    fixed __init__.py

--- pyarubaimc/__init__.py
+++ pyarubaimc/__init__.py
@@ -11,4 +11,4 @@ Copyright (c) 2022 Aruba, a Hewlett Packard Enterprise company
 
 version 0.1.0 - init release
 """
-from pyarubaimc import (alarms, device)
+from pyarubaimc.plat import (alarms, device)
