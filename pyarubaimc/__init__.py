9114bbfd654ab484c194bfaddba21a155ee6d393
Rick Kauffman <rick@rickkauffman.com>
2022-04-21 11:50:13 -0700

    minor document changes to init.py files2

--- pyarubaimc/__init__.py
+++ pyarubaimc/__init__.py
@@ -9,6 +9,6 @@ pyarubaimc is a python binding for working with Aruba IMC 7.3 or greater.
 
 Copyright (c) 2022 Aruba, a Hewlett Packard Enterprise company
 
-version 0.1.1 - init release.
+version 0.1.2 - beta.
 """
 from pyarubaimc.plat import (auth, objects)
