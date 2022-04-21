69a9d3088237593a566e5a25b38acf2cb9527d52
Rick Kauffman <rick@rickkauffman.com>
2022-04-21 08:16:08 -0700

    pypi

--- pyarubaimc/__init__.py
+++ pyarubaimc/__init__.py
@@ -2,14 +2,13 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright 2015 Hewlett Packard Enterprise Development LP
+Information
+==================
 
-Licensed under the Apache License, Version 2.0 (the “License”); you may not use this file except in
-compliance with the License. You may obtain a copy of the License at
+pyarubaimc is a python binding for working with Aruba IMC 7.3 or greater.
 
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software distributed
-under the License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS
-OF ANY KIND, either express or implied. See the License for the specific language governing
-permissions and limitations under the License.
+Copyright (c) 2022 Aruba, a Hewlett Packard Enterprise company
+
+version 0.1.0 - init release
 """
+from pyarubaimc import (alarms, device)
---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name = 'pyarubaimc',
+    packages = ['pyarubaimc'],
+    version = '0.1.0',
+    description = 'A python binding to work with the Aruba IMC API',
+    author = 'Rick Kauffman',
+    author_email = 'rick@rickkauffman.com',
+    url = 'https://github.com/aruba/pyarubaimc',
+    download_url = 'https://github.com/aruba/pyarubaimc/archive/refs/tags/v0.1.0.tar.gz',
+    keywords = ['IMC', 'api', 'python'],
+    install_requires=[
+          'requests',
+          'urllib3',
+          'nose',
+      ],
+    classifiers = [],
+)
